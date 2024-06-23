050f474b2c737d8193bac57f0bdc8169477e33a8
charm <charmniovi@tutanota.com>
2024-06-23 14:59:02 +0000

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
@@ -5,7 +6,7 @@ from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
@@ -13,9 +14,12 @@ def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(
+        asyncio.create_task(service.register_device(hue_light)),
+        asyncio.create_task(service.register_device(speaker)),
+        asyncio.create_task(service.register_device(toilet))
+    )
 
     # create a few programs
     wake_up_program = [
@@ -31,14 +35,14 @@ def main() -> None:
         Message(toilet_id, MessageType.CLEAN),
     ]
 
-    # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    # # run the programs
+    await asyncio.create_task(service.run_program(wake_up_program))
+    await asyncio.create_task(service.run_program(sleep_program))
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
